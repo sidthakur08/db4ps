5da1ce71d1c41b26f0d50ea5a936640b9252af3d
Aayush Agarwal <aayush.88@outlook.com>
2021-04-20 19:43:14 +0530

    added Login and SignUp

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+#protect from modifying cookies, cross-site eequest forgery attacks
+app.config['SECRET_KEY'] = 'd694936a4192c385995f1a67fd2af4aa'
 
 @app.route("/")
 def home():
     return render_template('home.html', title='home')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'aayush@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
