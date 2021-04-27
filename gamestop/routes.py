38eb5f70e2b2077186155947505440a5cfc76005
Siddhant Thakur <siddhantthakur08@gmail.com>
2021-04-27 19:21:29 +0530

    added game similarity

--- gamestop/routes.py
+++ gamestop/routes.py
@@ -3,7 +3,17 @@ from gamestop import app, db, bcrypt
 from gamestop.forms import RegistrationForm, LoginForm
 from gamestop.models import User, Game
 from flask_login import login_user, current_user, logout_user, login_required
-import subprocess
+from scipy import spatial
+import json
+import numpy as np
+
+# loading games data
+with open('./gamestop/games.txt','r') as f:
+    games = json.load(f)
+
+# loading the vector file
+with open('gamestop/game_vector.txt','r') as f:
+    game_vector = json.load(f)
 
 
 @app.route("/")
@@ -59,6 +69,35 @@ def account():
     return render_template('account.html', title='Account')
 
 
-@app.route("/similar",methods=['POST','GET'])
-def sim():
-    return render_template('similar.html', title='Similar Games')
+@app.route("/get_sim",methods = ['POST','GET'])
+def get_sim():
+    if request.method == 'GET':
+        return render_template('get_similar.html', title='Similar Games', sim_games = "Enter the name of a game to get similar games.")
+    
+    else:
+        name = [x for x in request.form.values()]
+
+        try:
+            def get_sim(a,b):
+                return 1-spatial.distance.cosine(game_vector[a],game_vector[b])
+        
+            def get_5_sim(to_search):
+                sim_measure = []
+                for i in game_vector.keys():
+                    try:
+                        sim_measure.append(get_sim(to_search,i))
+                    except:
+                        sim_measure.append(0)
+                sorted_sim_measure = np.argsort(sim_measure)[-6:-1][::-1]
+                sim_games = [(games[s]['Name'],sim_measure[s]) for s in sorted_sim_measure]
+                return sim_games
+            
+            sim_games = get_5_sim(name[0])
+
+            if sim_games == [('Final Fantasy XII: The Zodiac Age', 0), ('Final Fantasy VIII Remastered', 0), ('Final Fantasy VII Remake', 0), ('Final Fantasy VII', 0), ('Final Fantasy Type-0 HD', 0)]:
+                sim_games = False
+
+        except Exception as e:
+            sim_games = False
+
+        return render_template('display_similar.html', title='Similar Games', game_name = name[0],sim_games = sim_games)
